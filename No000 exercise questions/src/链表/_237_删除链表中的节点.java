90fddbff0cbe1aede164331ef400e10af5eaa156
HatcherCheung <1109079622@qq.com>
2021-08-29 09:18:30 +0800

    完成力扣的一道链表题

---
+++ No000 exercise questions/src/链表/_237_删除链表中的节点.java
@@ -0,0 +1,25 @@
+package 链表;
+
+/**
+ * https://leetcode-cn.com/problems/delete-node-in-a-linked-list/
+ * @author: HatcherCheung
+ * Date:  2021/8/28
+ */
+public class _237_删除链表中的节点 {
+
+    // 思路：用当前节点的下一个节点的val替换掉当前节点的val,再把当前节点的next修改为下一个节点的next。
+    // 等于是先覆盖值(val和next)，再删除当前节点的下一个节点
+
+    public static void main(String[] args) {
+
+    }
+    public void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+    class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) { val = x; }
+    }
+}
